5d6b15dd40bbc248ee4076b9ee6ecbdf54cab7f5
PCC <pingchungchang@gmail.com>
2024-12-30 16:09:46 +0800

    fixed random bug

--- backend/tools.js
+++ backend/tools.js
@@ -3,7 +3,8 @@ function Sign(k) {
 	return k>0?1:k<0?-1:0;
 }
 
-function RandInt(l,r) {
+function RandInt(l,r) {//get random int between [l,r];
+	r++;
 	return Math.floor(Math.random()*(r-l)+l);
 }
 
